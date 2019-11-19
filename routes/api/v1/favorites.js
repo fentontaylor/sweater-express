75d4c95a05cd0fcecfd1707388012902073b9ac7
fentontaylor <fentontaylor@gmail.com>
2019-11-18 20:03:35 -0700

    Rename routes from papers to favorites

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -7,13 +7,23 @@ const database = require('knex')(configuration);
 
 
 router.get('/', (request, response) => {
-  database('papers').select()
-    .then((papers) => {
-      response.status(200).json(papers);
+  database('favorites').select()
+    .then((favorites) => {
+      response.status(200).json(favorites);
     })
     .catch((error) => {
       response.status(500).json({ error });
     });
+});
+
+router.post('/', (request, response) => {
+  var key = request.body.key;
+  var location = request.body.location;
+
+  database('users').where('api_key', key)
+    .then(user => {
+      
+    })
 });
 
 module.exports = router;
