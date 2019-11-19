a9f779f92daa0d3531e372f023882269855549ec
fentontaylor <fentontaylor@gmail.com>
2019-11-18 20:34:31 -0700

    Add POST /api/v1/favorites, needs check for api_key and error handling

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -17,12 +17,17 @@ router.get('/', (request, response) => {
 });
 
 router.post('/', (request, response) => {
-  var key = request.body.key;
+  var key = request.body.api_key;
   var location = request.body.location;
 
   database('users').where('api_key', key)
     .then(user => {
-      
+      return database('favorites').insert({
+        user_id: user[0].id, location: location
+      })
+    })
+    .then(() => {
+      response.status(200).json({ message: `${location} has been added to your favorites`})
     })
 });
 
