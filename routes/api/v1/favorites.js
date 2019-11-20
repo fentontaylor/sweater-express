63f9b7f1f6b133953f8c7955f2ef3f1b7b774bfd
fentontaylor <fentontaylor@gmail.com>
2019-11-19 18:49:44 -0700

    Add try/catch to helper async functions

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -5,15 +5,33 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
-async function findUser(apiKey) {
-  let user = await database('users').where('api_key', apiKey);
-  return user;
+async function findUser (apiKey) {
+  try {
+    let user = await database('users').where('api_key', apiKey);
+    return user;
+  } catch (e) {
+    return e;
+  }
 }
 
-async function findFavorite(user, location) {
-  let favorite = await database('favorites')
-    .where({ user_id: user[0].id, location: location })
-  return favorite;
+async function findFavorite (user, location) {
+  try {
+    let favorite = await database('favorites')
+      .where({ user_id: user[0].id, location: location })
+    return favorite;
+  } catch (e) {
+    return e;
+  }
+}
+
+async function createFavorite (user, location) {
+  try {
+    let newFavorite = await database('favorites')
+      .insert({ user_id: user[0].id, location: location })
+    return newFavorite;
+  } catch (e) {
+    return e;
+  }
 }
 
 router.get('/', (request, response) => {
@@ -24,7 +42,7 @@ router.post('/', (request, response) => {
   var key = request.body.api_key;
   var location = request.body.location;
 
-  if (!key) { return response.status(401).send({ error: 'Invalid or missing API key' })}
+  if (!key) response.status(401).send({ error: 'Invalid or missing API key' })
 
   findUser(key)
     .then(user => {
@@ -32,21 +50,19 @@ router.post('/', (request, response) => {
         findFavorite(user, location)
           .then(favorite => {
             if (!favorite.length) {
-              return database('favorites').insert({
-                user_id: user[0].id, location: location
-              })
-              .then(() => {
-                response.status(200).json({ message: `${location} has been added to your favorites` })
-              })
+              createFavorite(user, location)
+                .then(() => {
+                  response.status(200).json({ message: `${location} has been added to your favorites` })
+                })
             } else {
-              return response.status(409).json({ message: `${location} is already in your favorites`})
+              response.status(409).json({ message: `${location} is already in your favorites`})
             };
           })
           .catch((error) => {
             response.status(500).json({ error });
           });
       } else {
-        return response.status(401).send({ error: 'Invalid or missing API key' })
+        response.status(401).send({ error: 'Invalid or missing API key' })
       };
     })
     .catch((error) => {
