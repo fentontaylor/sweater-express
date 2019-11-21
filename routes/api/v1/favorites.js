0b7c00a362e3bf4005f5aa409eed8a7fd12b9861
fentontaylor <fentontaylor@gmail.com>
2019-11-20 18:00:25 -0700

    Add DELETE /api/v1/favorites

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -6,6 +6,11 @@ const findUser = helpers.findUser;
 const findFavorite = helpers.findFavorite;
 const createFavorite = helpers.createFavorite;
 
+// Delete this after moving to asyncHelpers
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../../knexfile')[environment];
+const database = require('knex')(configuration);
+
 router.get('/', (request, response) => {
   
 });
@@ -42,4 +47,20 @@ router.post('/', (request, response) => {
     });
 });
 
+router.delete('/', (request, response) => {
+  var body = request.body;
+  var location = body.location;
+  var key = body.api_key;
+
+  if (!key) { return response.status(401).send({ error: 'Invalid or missing API key' }) };
+  if (!location) { return response.status(422).send({ error: "Missing required 'location' property"}) };
+
+  findUser(key)
+    .then(user => {
+      database('favorites').where({
+        user_id: user[0].id, location: location
+      }).del()
+        .then(response.status(204).send())
+    })
+})
 module.exports = router;
