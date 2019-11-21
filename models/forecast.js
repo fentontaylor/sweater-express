6b92aad1fe373355e655103ca47ff03a7736429d
fentontaylor <fentontaylor@gmail.com>
2019-11-21 15:06:37 -0700

    Add Currently model an use in Forecast model

--- models/forecast.js
+++ models/forecast.js
@@ -1,34 +1,17 @@
+const Currently = require('./currently')
+const Daily = require('./daily')
+
 class Forecast {
   constructor(city, forecast) {
     this.location = city;
-    this.currentWeather = this.currently(forecast.currently);
+    this.currentWeather = new Currently(forecast.currently);
     this.daily = this.dailyForecast(forecast.daily);
     this.hourly = this.hourlyForecast(forecast.hourly);
   }
 
-  currently(fc) {
-    return {
-      summary: fc.summary,
-      icon: fc.icon,
-      precipIntensity: fc.precipIntensity,
-      precipProbability: fc.precipProbability,
-      temperature: fc.temperature,
-      humidity: fc.humidity,
-      pressure: fc.pressure,
-      windSpeed: fc.windSpeed,
-      windGust: fc.windGust,
-      windBearing: fc.windBearing,
-      cloudCover: fc.cloudCover,
-      visibility: fc.visibility
-    }
-  }
-
   dailyForecast(fc) {
-    return {
-      summary: fc.summary,
-      icon: fc.icon,
-      data: fc.data.slice(0,7)
-    }
+    let dailyData = fc.data.slice(0, 7)
+    return dailyData.map(data => new this.daily(data))
   }
 
   hourlyForecast(fc) {
