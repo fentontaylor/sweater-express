7c19938315eb2c914d079d5513f8c981b89ce5f4
fentontaylor <fentontaylor@gmail.com>
2019-11-20 21:45:55 -0700

    Add format function to favForecast model to return only required info

--- models/favForecast.js
+++ models/favForecast.js
@@ -1,8 +1,26 @@
 class favForecast {
   constructor(city, forecast) {
     this.location = city,
-    this.current_weather = forecast['currently']
+    this.current_weather = this.format(forecast.currently)
+  }
+
+  format(fc) {
+    return {
+      summary: fc.summary,
+      icon: fc.icon,
+      precipIntensity: fc.precipIntensity,
+      precipProbability: fc.precipProbability,
+      temperature: fc.temperature,
+      humidity: fc.humidity,
+      pressure: fc.pressure,
+      windSpeed: fc.windSpeed,
+      windGust: fc.windGust,
+      windBearing: fc.windBearing,
+      cloudCover: fc.cloudCover,
+      visibility: fc.visibility
+    }
   }
 }
 
 module.exports = favForecast;
+
